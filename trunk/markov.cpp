f8c5bdba9045999c32e79ca6dc60a730476b64c2
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-03-22 17:23:38 +0000

    implement end effects using reactivity ratios.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@2 162ca379-00fc-49ab-b88d-9b166cbd90eb

--- trunk/markov.cpp
+++ trunk/markov.cpp
@@ -1,3 +1,4 @@
+//$Id$
 #include <iostream>
 #include <fstream>
 #include <cstdlib> // for rand() and srand()
@@ -10,7 +11,9 @@ int main()
     srand(time(0)); // generate seed for random number
     int chains = 10000; // number of polymer chains
     int length = 1000; //length of each polymer chain
-    float p_A, p_B;
+    float p_a, p_b; //single-monomer unconditional probabilities
+    float p_aa, p_ab, p_ba, p_bb; //diad unconditional probabilities
+    float p_aga, p_agb, p_bga, p_bgb; //conditional probabilities
     float r1, r2, x;
     ofstream myfile;
     char* outfile;
@@ -19,24 +22,48 @@ int main()
     cin >> outfile;
     cout << "Enter reactivity ratios r1, r2, and input ratio x = [A]/[B]: ";
     cin >> r1 >> r2 >> x;
-    p_A = (x + r1*x*x)/(r1*x*x + 2*x + r2);
-    p_B = (x + r2)/(r1*x*x + 2*x + r2);
+    
+    //calculate the probabilites from the input parameters
+    p_a = (x + r1*x*x)/(r1*x*x + 2*x + r2);
+    p_b = (x + r2)/(r1*x*x + 2*x + r2);
+    p_agb = 1/(1+r2);
+    p_bga = 1/(1+r1);
+    p_aga = 1-p_bga;
+    p_bgb = 1-p_agb;
+    
+    //write the header information
     myfile.open(outfile);
     myfile << "r1=" << r1 << " r2=" << r2 << " [A]/[B]=" << x << endl;
-    myfile << "p_A=" << p_A << " p_B=" << p_B << endl;
+    myfile << "p_a=" << p_a << " p_b=" << p_b << endl;
+    myfile << "p_aga=" << p_aga << " p_agb=" << p_agb << " p_bga=" << p_bga << " p_bgb=" << p_bgb << endl;
     myfile << "[A]/[B] = " << x << endl;
+    
     for (int i=0; i<=chains; i++)
     {
 	int monomer = 0;
+	int monomer_a = 0;
         for (int j=0; j<=length; ++j)
 	{
-	   float k = float(rand())/RAND_MAX;
-	    if (k < p_A)
+//	    myfile << monomer << ", ";
+	    float k = float(rand())/RAND_MAX;
+	    if (monomer == 0 && k < p_agb)
 	    {
-	    monomer++;
+     		monomer = 1;
+            monomer_a++;;
 	    }
+	    else if (monomer == 1 && k < p_aga)
+	    {
+		    monomer = 1;
+            monomer_a++;;
+	    }
+	    else
+	    {
+   		    monomer = 0;
+        }
+
+//	myfile << monomer << endl;
 	}
-	myfile << monomer << endl;
+	myfile << monomer_a << endl;
     }
     myfile.close();
 	
