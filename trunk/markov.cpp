61aed6a7a6baf5b3005d58ec32c56997546f71c0
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-03-28 19:38:36 +0000

    Remove the results from each individual chain from the output, and print out only the summary statistics.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@11 162ca379-00fc-49ab-b88d-9b166cbd90eb

--- trunk/markov.cpp
+++ trunk/markov.cpp
@@ -22,11 +22,11 @@ int main()
     float a_mean, aa_mean, ab_mean, ba_mean, bb_mean;   
     float a_var, aa_var, ab_var, ba_var, bb_var;
     int bad;
-    ofstream myfile, myfile2;
-    string outfile, statsfile;
+    ofstream myfile;
+    string outfile;
 
     cout << "File name for output ";
-    cin >> outfile; statsfile = outfile;
+    cin >> outfile;
     cout << "Enter reactivity ratios r1, r2, and input ratio x = [A]/[B]: ";
     cin >> r1 >> r2 >> x;
     
@@ -39,19 +39,12 @@ int main()
     p_bgb = 1-p_agb;
     
     //write the header information
-    statsfile += "_stats.dat";
-    outfile += ".dat";
     myfile.open(outfile.c_str());
-    myfile2.open(statsfile.c_str());
     myfile << "Created by $Id$" << endl;
     myfile << "r1=" << r1 << " r2=" << r2 << " [A]/[B]=" << x << endl;
-    myfile2 << "Created by $Id$" << endl;
-    myfile2 << "r1=" << r1 << " r2=" << r2 << " [A]/[B]=" << x << endl;
     myfile << "p_a=" << p_a << " p_b=" << p_b << endl;
     myfile << "p_aga=" << p_aga << " p_agb=" << p_agb << " p_bga=" << p_bga << " p_bgb=" << p_bgb << endl;
     
-    //column titles
-    myfile << "chain #, a, aa, ab, ba, bb" << endl; 
     //Main body of the program
     a_mean = aa_mean = ab_mean = ba_mean = bb_mean = 0;   
     a_var = aa_var = ab_var = ba_var = bb_var = 0;
@@ -96,18 +89,16 @@ int main()
                }
         assert(bad);       
         }
-        myfile << i << ", " << monomer_a << ", " << aa << ", " << ab << ", " << ba << ", " << bb << endl;
         a_mean+=monomer_a; aa_mean+=aa; ab_mean+=ab; ba_mean+=ba; bb_mean+=bb;
         a_var+=monomer_a*monomer_a; aa_var+=aa*aa; ab_var+=ab*ab; ba_var+=ba*ba;bb_var+=bb*bb; 
     }
     a_mean /= chains; aa_mean /= chains; ab_mean /= chains; ba_mean /= chains; bb_mean /= chains;
     a_var = a_var/chains - (a_mean*a_mean); aa_var = aa_var/chains - (aa_mean*aa_mean); ab_var = ab_var/chains - (ab_mean*ab_mean);
     ba_var = ba_var/chains - (ba_mean*ba_mean); bb_var = bb_var/chains - (bb_mean*bb_mean);
-    myfile2 << "mean, " << a_mean << ", " << aa_mean << ", " << ab_mean << ", " << ba_mean << ", " << bb_mean << endl;
-    myfile2 << "std, " << sqrt(a_var) << ", " << sqrt(aa_var) << ", " << sqrt(ab_var) << ", " << sqrt(ba_var) << ", " << sqrt(bb_var) << endl;
+    myfile << "mean, " << a_mean << ", " << aa_mean << ", " << ab_mean << ", " << ba_mean << ", " << bb_mean << endl;
+    myfile << "std, " << sqrt(a_var) << ", " << sqrt(aa_var) << ", " << sqrt(ab_var) << ", " << sqrt(ba_var) << ", " << sqrt(bb_var) << endl;
     
     myfile.close();
-    myfile2.close();
     return 0;
 }
 
