fc5b1a78c3bc8b02c1ae876e1ea086ba22b352e6
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-04-25 14:47:58 +0000

    Start the deteriminstic numerical solution to the Mayo-Lewis equation.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@25 162ca379-00fc-49ab-b88d-9b166cbd90eb

---
+++ trunk/deterministic.cpp
@@ -0,0 +1,20 @@
+//$Id$
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+int main(int r1, int r2)
+{
+  float x, y;
+  float dx, dy;
+  float r1, r2;
+
+  for (int i = 1; i++; i<=1e4)
+  {
+  }
+}
+
