64e0c2fcee28d58917795e0c90875f74bfaa23d0
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-05-08 22:00:00 +0000

    This seems to work for integration over 100 steps, but breaks for anything else. I think i'm on the right track, but I'm doing something wrong.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@26 162ca379-00fc-49ab-b88d-9b166cbd90eb

--- trunk/deterministic.cpp
+++ trunk/deterministic.cpp
@@ -7,14 +7,37 @@
 
 using namespace std;
 
-int main(int r1, int r2)
+int main()
 {
-  float x, y;
-  float dx, dy;
-  float r1, r2;
+  float x; //beginning ratio of m1/m2
+  float r1, r2; //reactivity ratios, input from user
+  float m1, m2, dm1, dm2; //actual monomer compositions and differential
+  float m10, m20; //initial compositions
+  int steps = 100; //number of steps to integrate over
+  string outfile; 
+  ofstream myfile;
 
-  for (int i = 1; i++; i<=1e4)
+  cout << "Name for output file ";
+  cin >> outfile; 
+  cout << "Enter reactivity ratios r1, r2, and input ratio x = [A]/[B]: ";
+  cin >> r1 >> r2 >> x;
+
+  //convert the input ratio into monomer compositions
+  m1 = m10 = x/(1.0+x);
+  m2 = m20 = 1.0/(1.0+x);
+
+  myfile.open(outfile.c_str());
+  myfile << "Time, c_m1, c_m2" << endl;
+  for (int i=0; i<=steps; i++)
   {
+	  dm2 = -m2*float(i)/steps;
+	  m2 += dm2; 
+	  dm1 = dm2*x*(r1*m1 + m2)/(m1 + r2*m2);
+	  m1 += dm1;
+	  assert(m2 >= 0);
+	  assert(m1 >= 0);
+	  myfile << i << ", " <<  m1 << ", " <<  m2 << endl;
+
   }
 }
 
