6c60dd22b14cd1037061f21dd053b76b0c0c6eda
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-05-09 21:56:48 +0000

    Attempt to make the deterministic version use runge kutta to integrate. It compiles, but it chokes on the block copolymer, and doesn't appear to give correct results.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@27 162ca379-00fc-49ab-b88d-9b166cbd90eb

--- trunk/deterministic.cpp
+++ trunk/deterministic.cpp
@@ -9,13 +9,16 @@ using namespace std;
 
 int main()
 {
+  int steps = 1000; //number of steps to integrate over
+  float h; //time increments
   float x; //beginning ratio of m1/m2
   float r1, r2; //reactivity ratios, input from user
   float m1, m2, dm1, dm2; //actual monomer compositions and differential
-  float m10, m20; //initial compositions
-  int steps = 100; //number of steps to integrate over
+  float k11, k12, k21, k22;
   string outfile; 
   ofstream myfile;
+  float f1(float m1, float m2, float r1);
+  float f2(float m1, float m2, float r2);
 
   cout << "Name for output file ";
   cin >> outfile; 
@@ -23,21 +26,36 @@ int main()
   cin >> r1 >> r2 >> x;
 
   //convert the input ratio into monomer compositions
-  m1 = m10 = x/(1.0+x);
-  m2 = m20 = 1.0/(1.0+x);
+  m1 = x/(1.0+x);
+  m2 = 1.0/(1.0+x);
 
+  outfile += ".dat";
   myfile.open(outfile.c_str());
+  myfile << "Created by $Id$"<< endl;
   myfile << "Time, c_m1, c_m2" << endl;
   for (int i=0; i<=steps; i++)
   {
-	  dm2 = -m2*float(i)/steps;
-	  m2 += dm2; 
-	  dm1 = dm2*x*(r1*m1 + m2)/(m1 + r2*m2);
-	  m1 += dm1;
+	  h = float(i)/steps;
+	  k11 = h*f1(m1, m2, r1);
+	  k12 = h*f2(m1, m2, r2);
+	  k21 = h*f1(m1+k11/2, m2+k12/2, r1);
+	  k22 = h*f2(m1+k11/2, m2+k12/2, r2);
+	  m1 += k21;
+	  m2 += k22;
 	  assert(m2 >= 0);
 	  assert(m1 >= 0);
 	  myfile << i << ", " <<  m1 << ", " <<  m2 << endl;
 
   }
+}
+
+float f1(float m1, float m2, float r1)
+{
+  return -(m1)*(r1*m1 + m2);
+}
+
+float f2(float m1, float m2, float r2)
+{
+  return -(m2)*(r2*m2 + m1);
 }
 
