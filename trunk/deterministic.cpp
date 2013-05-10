0c83ce7c56b25f33ac8e864a5642051de737b1b1
cmcfaul <cmcfaul@162ca379-00fc-49ab-b88d-9b166cbd90eb>
2013-05-10 21:05:17 +0000

    Re-write runge kutta lines to use arrays instead of repeated variables.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/copolymer@28 162ca379-00fc-49ab-b88d-9b166cbd90eb

--- trunk/deterministic.cpp
+++ trunk/deterministic.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <cassert>
 #include <cmath>
+//#include "rk4.c" //4th order runge kutta routine from Landau & Paez
 
 using namespace std;
 
@@ -12,50 +13,46 @@ int main()
   int steps = 1000; //number of steps to integrate over
   float h; //time increments
   float x; //beginning ratio of m1/m2
-  float r1, r2; //reactivity ratios, input from user
-  float m1, m2, dm1, dm2; //actual monomer compositions and differential
-  float k11, k12, k21, k22;
+  float r[2]; //reactivity ratios, input from user
+  float m[2], dm[2]; //actual monomer compositions and differential
+  float m0[2];
+  float k1[2], k2[2]; 
   string outfile; 
   ofstream myfile;
-  float f1(float m1, float m2, float r1);
-  float f2(float m1, float m2, float r2);
+  float f(float m[2], float r[2], int i);
 
   cout << "Name for output file ";
   cin >> outfile; 
   cout << "Enter reactivity ratios r1, r2, and input ratio x = [A]/[B]: ";
-  cin >> r1 >> r2 >> x;
+  cin >> r[0] >> r[1] >> x;
 
   //convert the input ratio into monomer compositions
-  m1 = x/(1.0+x);
-  m2 = 1.0/(1.0+x);
+  m[0] = m[0] = x/(1.0+x);
+  m[1] = m[1] = 1.0/(1.0+x);
 
   outfile += ".dat";
   myfile.open(outfile.c_str());
   myfile << "Created by $Id$"<< endl;
-  myfile << "Time, c_m1, c_m2" << endl;
+  myfile << "Time, c_m1, c_m2, f_m1, f_m2, f_total" << endl;
   for (int i=0; i<=steps; i++)
   {
 	  h = float(i)/steps;
-	  k11 = h*f1(m1, m2, r1);
-	  k12 = h*f2(m1, m2, r2);
-	  k21 = h*f1(m1+k11/2, m2+k12/2, r1);
-	  k22 = h*f2(m1+k11/2, m2+k12/2, r2);
-	  m1 += k21;
-	  m2 += k22;
-	  assert(m2 >= 0);
-	  assert(m1 >= 0);
-	  myfile << i << ", " <<  m1 << ", " <<  m2 << endl;
+	  k1[0] = m[0] + 0.5*h*f(m, r, 0);
+	  k1[1] = m[1] + 0.5*h*f(m, r, 1);
+	  k2[0] = h*f(k1, r, 0);
+	  k2[1] = h*f(k1, r, 1);
+	  m[0] += k2[0];
+	  m[1] += k2[1];
+	  assert(m[1] >= 0);
+	  assert(m[0] >= 0);
+	  myfile << i << ", " <<  m[0] << ", " <<  m[1] << ", ";
+	  myfile << (m0[0]-m[0])/m0[0] << ", " << (m0[1]-m[1])/m0[1] << ", " << 1-m[1]-m[0] << endl;
 
   }
 }
 
-float f1(float m1, float m2, float r1)
+float f(float m[2], float r[2], int i)
 {
-  return -(m1)*(r1*m1 + m2);
+  if (i == 0) return -(m[0])*(r[0]*m[0] + m[1]);
+  if (i == 1) return -(m[1])*(r[1]*m[1] + m[0]);
 }
-
-float f2(float m1, float m2, float r2)
-{
-  return -(m2)*(r2*m2 + m1);
-}
-
